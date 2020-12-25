795e6795e5164dacd8c23549dc2a2a698405916e
Rushali Sarkar <rushalisarkar2000@gmail.com>
2020-12-25 11:27:02 +0530

    Added the range class in c sharp

---
+++ csharp/rangeModified.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Range {
+
+    public class Range {
+
+        protected int start;
+        protected int end;
+
+        protected void reset() {
+            this.start = 0;
+            this.end = 0;
+            return;
+        } 
+
+
